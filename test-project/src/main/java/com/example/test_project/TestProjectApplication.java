68450b5dbf188e6d74fb909d1e40c73d2f049468
darvycg <darvy.ceron@gmail.com>
2025-02-08 12:09:43 -0600

    Add first basic test to ensure pom is working

--- test-project/src/main/java/com/example/test_project/TestProjectApplication.java
+++ test-project/src/main/java/com/example/test_project/TestProjectApplication.java
@@ -1,13 +1,23 @@
 package com.example.test_project;
 
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.microsoft.playwright.*;
 
 @SpringBootApplication
 public class TestProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestProjectApplication.class, args);
+		try (Playwright playwright = Playwright.create()) {
+			Browser browser = playwright.chromium().launch();
+			Page page = browser.newPage();
+			page.navigate("https://playwright.dev/");
+			page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+		}
+		// SpringApplication.run(TestProjectApplication.class, args);
 	}
 
 }
