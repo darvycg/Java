81bb6a2c285bc091d74c66bcc889a0bcd6f6821e
Darvy <darvy.ceron@gmail.com>
2025-10-11 19:59:40 -0500

    Remove the main application playwright test

--- test-project/src/main/java/com/learning/test_project/TestProjectApplication.java
+++ test-project/src/main/java/com/learning/test_project/TestProjectApplication.java
@@ -10,19 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.nio.file.Paths;
 
 @SpringBootApplication
-public class TestProjectApplication implements CommandLineRunner {
+public class TestProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.webkit().launch();
-            Page page = browser.newPage();
-            page.navigate("https://playwright.dev/");
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
-        }
-    }
 }
